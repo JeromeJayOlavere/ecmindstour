35180bbaf5cde376cc2e5b307510ee8eb51711d0
net4k <kompanyero24@gmail.com>
2024-03-08 09:32:53 +0800

    Changes to be committed: 	modified:   .env 	modified:   app/Views/welcome_message.php 	new file:   public/Images/DOT.png 	new file:   public/Images/EC MINDS TOUR LOGO-1.png 	new file:   public/Images/EC MINDS TOUR LOGO.png 	new file:   public/Images/LOGO BUS 2.png 	new file:   public/Images/logo1 PNG.png 	new file:   writable/logs/log-2024-03-08.log

--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Welcome to CodeIgniter 4!</title>
+    <title>Welcome to EC Minds Tour</title>
     <meta name="description" content="The small framework with powerful features">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" type="image/png" href="/favicon.ico">
@@ -225,7 +225,7 @@
 
     <div class="heroe">
 
-        <h1>Welcome to CodeIgniter <?= CodeIgniter\CodeIgniter::CI_VERSION ?></h1>
+        <h1>Welcome to EC Minds Tour </h1>
 
         <h2>The small framework with powerful features</h2>
 
@@ -295,15 +295,8 @@
 <!-- FOOTER: DEBUG INFO + COPYRIGHTS -->
 
 <footer>
-    <div class="environment">
 
-        <p>Page rendered in {elapsed_time} seconds</p>
-
-        <p>Environment: <?= ENVIRONMENT ?></p>
-
-    </div>
-
-    <div class="copyrights">
+ <div class="copyrights">
 
         <p>&copy; <?= date('Y') ?> CodeIgniter Foundation. CodeIgniter is open source project released under the MIT
             open source licence.</p>
